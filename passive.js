8a29e207d8c31c85f500987b52fe2a92e563ba75
Aryan Prodduturi <32280917+SmashMineGame@users.noreply.github.com>
2022-08-02 22:02:31 -0700

    formatting updates

--- passive.js
+++ passive.js
@@ -1,13 +1,11 @@
-
 const argv = process.argv.slice(2);
 const argc = argv.length;
 
-const if_undef = (i, d) => i == undefined ? d : parseInt(i);
-let months = if_undef(argv[0], 12);
+const if_undef = (i, d) => (i == undefined ? d : parseInt(i));
+let months = if_undef(argv[0], 120);
 let investment_per_month = if_undef(argv[1], 50);
-let principle = if_undef(argv[2], 0)
+let principle = if_undef(argv[2], 0);
 let rate_by_year = if_undef(argv[3], 1.12);
-
 
 const dollars = (d) => d.toFixed(2);
 
@@ -24,7 +22,7 @@ let profit_rate = (Math.pow(r, months + 1) - r) / (r - 1);
 let money_final = profit_rate * investment_per_month + principle_growth;
 
 let money_invested = investment_per_month * months + principle;
-console.log('Money Final:    ', dollars(money_final));
-console.log('Money Invested: ', dollars(money_invested));
-console.log('--------------------------');
-console.log('Profit:          ', dollars(money_final - money_invested));
+console.log('Money Final:    $', dollars(money_final));
+console.log('Money Invested: $', dollars(money_invested));
+console.log('---------------------------');
+console.log('Profit:         $ ', dollars(money_final - money_invested));
